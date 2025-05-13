a09b2acc0e2584e5f3b0fb1a2feabc9068918830
Ascrion <navodiths@gmail.com>
2025-05-13 11:21:47 +0530

    Backup after MVP code completion

--- library.py
+++ library.py
@@ -1,12 +1,13 @@
 """The Script acts as a Library Database Management System using Python with Postgress SQL"""
 
+import json
 import datetime as dt
+import csv
 
 import psycopg2
 
-
 # SQL Control
-def sql_controller(table, command, t_data):
+def sql_controller(table, command, t_data,settings):
     """Used to Control the SQL Database"""
     with psycopg2.connect(
         dbname="library",
@@ -27,7 +28,9 @@ def sql_controller(table, command, t_data):
                 """CREATE TABLE IF NOT EXISTS users (
                         id SERIAL PRIMARY KEY,
                         User_Name TEXT NOT NULL,
-                        Contact INT UNIQUE NOT NULL
+                        Contact INT UNIQUE NOT NULL,
+                        Books_Borrowed JSONB,
+                        Fines DECIMAL 
                         );"""
             )
 
@@ -43,32 +46,23 @@ def sql_controller(table, command, t_data):
                         );"""
             )
 
-            # cur.execute(
-            #     """CREATE TABLE IF NOT EXISTS transactions (
-            #             id SERIAL PRIMARY KEY,
-            #             book_id INT REFERENCES books(id),
-            #             user_id INT REFERENCES users(id),
-            #             Borrow_Date DATE NOT NULL,
-            #             Return_Date DATE
-            #             );"""
-            # )
-
-            # cur.execute(
-            #     """CREATE TABLE IF NOT EXISTS fines (
-            #             id SERIAL PRIMARY KEY,
-            #             book_id INT REFERENCES books(id),
-            #             user_id INT REFERENCES users(id),
-            #             borrow_date INT REFERENCES books(Borrow_Date),
-            #             return_date INT REFERENCES users(Return_Date),
-            #             Amount INT
-            #             );"""
-            # )
+            cur.execute(
+                """CREATE TABLE IF NOT EXISTS transactions (
+                        id SERIAL PRIMARY KEY,
+                        book_id INT REFERENCES books(id),
+                        user_id INT REFERENCES users(id),
+                        Issue_Date DATE NOT NULL,
+                        Return_Date DATE,
+                        Actual_Return_Date DATE,
+                        Transaction TEXT NOT NULL
+                        );"""
+            )
 
             # User Operations
             if table == "users":
                 # Check if user exists from before
                 cur.execute(
-                    f"SELECT id,User_Name FROM users WHERE Contact = %s",
+                    f"SELECT * FROM users WHERE Contact = %s",
                     (data["Contact"],),
                 )
                 result = cur.fetchall()
@@ -111,7 +105,29 @@ def sql_controller(table, command, t_data):
                         if result == []:
                             return("User does not exist")
                         else:
-                            return(result[0][1])
+                            note = {
+                                'User_Name': result[0][1],
+                                'Contact': result[0][2],
+                                'Books_Borrowed': result[0][3],
+                                'Fines': result[0][4]
+                            }
+                            return(note)
+                        
+                    case 5: # Pay Fines
+                        if result == []:
+                            return("User does not exist")
+                        else:
+                            print(f"{result[0][1]} has {result[0][4]} in fines.")
+                            if not test:
+                                if result[0][4]== None:
+                                    prev_fine = 0
+                                else:
+                                    prev_fine = result[0][4]
+                                x = float(input("How much of the fine do you want to pay now?"))
+                                updated_fine = prev_fine - x 
+                                f"UPDATE users SET Fines = {updated_fine} WHERE id = {result[0][0]}"
+                                return(f"{result[0][1]} has paid off {x}, leaving {updated_fine} in fines.")
+
 
                     case _:  # Default case
                         return("Invalid Operation")
@@ -140,8 +156,8 @@ def sql_controller(table, command, t_data):
                                         "Enter location of the copy (ex - R1C1) "
                                     ),
                                 }
-                                additional_data["Available"] = additional_data["Copies"]
                                 data.update(additional_data)
+                            data["Available"] = data["Copies"]
 
                             keys = ", ".join(data.keys())
                             placeholders = ", ".join(["%s"] * len(data))
@@ -156,7 +172,7 @@ def sql_controller(table, command, t_data):
                         else:
                             print("Book already exists, updating counts")
                             new_Copies = data["Copies"] + result[0][1]
-                            new_Available = data["Availiable"] + result[0][2]
+                            new_Available = data["Available"] + result[0][2]
 
                             if new_Copies <= 0:
                                 print(
@@ -166,6 +182,7 @@ def sql_controller(table, command, t_data):
                                 cur.execute(
                                     f"UPDATE books SET Copies = {new_Copies}, Available = {new_Available} WHERE id = {result[0][3]}"
                                 )
+                            return(f"{data['Book_Name']} updated")
 
                     case 2:  # Delete
                         if result == []:
@@ -182,7 +199,6 @@ def sql_controller(table, command, t_data):
                                 "Book_Name": result[0][0],
                                 "Copies": result[0][1],
                                 "Available": result[0][2],
-                                "id": result[0][3],
                                 "Location": result[0][4],
                                 "user_id": result[0][5],
                             }
@@ -190,6 +206,128 @@ def sql_controller(table, command, t_data):
 
                     case _:
                         return("Invalid Input")
+                    
+            # Transactions
+            if table == "transactions":
+
+                match command:
+                    
+                    case 1: # Borrow
+                        cur.execute(f"SELECT id,Available,user_id FROM books WHERE Book_Name = %s",(data["Book_Name"],),)
+                        rows1 = cur.fetchone()
+                        cur.execute(f"SELECT id, User_Name,Books_Borrowed,Fines FROM users WHERE Contact = %s",(data["Contact"],),)
+                        rows2 = cur.fetchone()
+
+                        borrow_date = dt.date.today()
+                        return_date = borrow_date + dt.timedelta(days=settings['Issue_Duration'])
+                        borrow_str = borrow_date.strftime("%Y-%m-%d")
+                        return_str = return_date.strftime("%Y-%m-%d")
+
+                        transaction = "Issue"
+
+                        # Check constraints
+                        if rows1 == None:
+                            return("Book does not exist")
+
+                        if rows2 == None:
+                            return("User does not exist")
+
+                        try:
+                            books_borrowed = json.loads(rows2[2])
+                        except:
+                            books_borrowed = []
+
+                        if data['Book_Name'] in books_borrowed:
+                            return("User has already borrowed the same book. Issue Denied.")
+                        
+                        if rows1[1]<1:
+                            return("Book is not available to issue")
+                        
+                        if len(books_borrowed) >= settings["Max_Book_Issues"]:
+                            return("User has issued max number of books possible")
+
+                        cur.execute("INSERT INTO transactions "
+                                "(book_id, user_id,Issue_Date,Return_Date,Transaction)" \
+                                " VALUES (%s, %s,%s,%s,%s)", 
+                                (rows1[0], rows2[0],borrow_str,return_str,transaction))
+                        
+                        available = rows1[1]
+                        available = available -1 
+                        books_borrowed.append(data['Book_Name'])
+                        
+                        try:
+                            book_users = json.loads(rows1[2])
+                        except:
+                            book_users = []
+                        book_users.append(rows2[1])
+
+                        cur.execute(
+                                "UPDATE books SET Available = %s, user_id = %s WHERE id = %s",
+                                (available, json.dumps(book_users), rows1[0])
+                            )
+                        cur.execute(
+                                "UPDATE users SET Books_Borrowed = %s WHERE id = %s",
+                                (json.dumps(books_borrowed), rows2[0])
+                            )
+                        return(f"{data['Book_Name']} has been borrowed by {rows2[1]}")
+
+                    case 2: # Return
+                        cur.execute(f"SELECT id,Available,user_id FROM books WHERE Book_Name = %s",(data["Book_Name"],),)
+                        rows1 = cur.fetchone()
+                        cur.execute(f"SELECT id, User_Name,Books_Borrowed,Fines FROM users WHERE Contact = %s",(data["Contact"],),)
+                        rows2 = cur.fetchone()
+
+                        cur.execute(
+                            "UPDATE transactions SET transaction = %s, Actual_Return_Date = %s WHERE book_id = %s AND user_id = %s",
+                            ('return', dt.date.today(), rows1[0], rows2[0])
+                        )
+                        try: #handle error if rows2[2] is a list instead of json
+                            books_borrowed = json.loads(rows2[2])
+                        except:
+                            books_borrowed = rows2[2]
+
+                        try:
+                            books_borrowed.remove(data['Book_Name'])
+                        except:
+                            return f"{data['Book_Name']} has not been issued or has already been returned."
+                        available = rows1[1]
+                        available = available +1
+
+                        try: #handle error if rows2[2] is a list instead of json
+                            book_users = json.loads(rows1[2])
+                        except:
+                            book_users=rows1[2]
+                        book_users.remove(rows2[1])
+
+                        cur.execute(
+                                "UPDATE books SET Available = %s, user_id = %s WHERE id = %s",
+                                (available, json.dumps(book_users), rows1[0])
+                            )
+                        cur.execute(
+                                "UPDATE users SET Books_Borrowed = %s WHERE id = %s",
+                                (json.dumps(books_borrowed), rows2[0])
+                            )
+
+                        # Fine Calculations
+                        cur.execute(f"SELECT Return_Date FROM transactions WHERE book_id = {rows1[0]} and user_id = {rows2[0]}")
+                        rows3 = cur.fetchone()
+
+                        borrowed_date = rows3[0] 
+                        fine = (dt.date.today() - borrowed_date).days * settings["Fines_Per_Day"]
+
+
+                        if rows2[3] == None:
+                            prev_fine = 0
+                        else:
+                            prev_fine = rows2[3]
+                        new_fine = prev_fine + fine
+
+                        cur.execute(f"UPDATE users SET Fines = {new_fine} WHERE id = {rows2[0]}")
+                        return(f"{data['Book_Name']} has been returned by {rows2[1]} with fine of {fine}")
+                        
+                    case _:
+                        return("Invalid Input")
+                
 
 
 # Librarian Menu
@@ -200,16 +338,31 @@ while __name__=="__main__":
         "\n 1. Issue / Return a book"
         "\n 2. Manage Books"
         "\n 3. Manage Users"
-        "\n 4. Manage Fines "
+        "\n 4. Settings "
         "\n 5. Export Records"
-        "\n 6. Settings"
         "\n 0. Exit"
     )
 
     admin = int(input("What is your Choice? (Enter number): "))
 
+    with open("./config.json", "r") as config_file:
+        settings = json.load(config_file)
+
     match admin:
-        case 2:
+        case 1: # Manage Transactions
+            table = "transactions"
+            command = int(
+                input(
+                    "1. Issue Books, 2. Return Books "
+                )
+            )
+            data = {
+                "Book_Name": input("Enter Book Name "),
+                "Contact": input("Enter Contact Number "),
+                'test': False,
+            }
+            print(sql_controller(table, command, data,settings))
+        case 2: # Manage Book Data
             table = "books"
             command = int(
                 input(
@@ -220,22 +373,131 @@ while __name__=="__main__":
                 "Book_Name": input("Enter Book Name "),
                 'test': False,
             }
-            print(sql_controller(table, command, data))
-        case 3:
+            print(sql_controller(table, command, data,settings))
+
+        case 3: # Manage User Data
             table = "users"
             command = int(
-                input("1. Add User, 2. Modify User, 3. Delete User 4. Read User ")
+                input("1. Add User, 2. Modify User, 3. Delete User, 4. Read User, 5. Pay Fines ")
             )
             data = {
                 "User_Name": input("Enter User Name "),
                 "Contact": input("Enter Contact "),
                 'test': False,
             }
-            print(sql_controller(table, command, data))
+            print(sql_controller(table, command, data,settings))
 
-        case 0:
+        case 0: # Exit out of CLI
             print("\n Exiting")
             break
+
+        case 4: # Settings Management
+            with psycopg2.connect(
+                dbname="library",
+                user="postgres",
+                password="postgress",
+                host="localhost",
+                port="5432",
+            ) as conn:
+                with conn.cursor() as cur:
+                    
+                    cur.execute(
+                    """CREATE TABLE IF NOT EXISTS settings (
+                            id SERIAL PRIMARY KEY,
+                            Librarian_Name TEXT NOT NULL,
+                            Issue_Duration INT NOT NULL,
+                            Fines_Per_Day DECIMAL NOT NULL,
+                            Max_Book_Issues_Per_User INT NOT NULL,
+                            Password TEXT NOT NULL,
+                            Date_Time TIMESTAMP NOT NULL
+                            );"""
+                    )
+                    
+                    # Get last settings to compare passwords for authentication 
+                    cur.execute("SELECT * FROM settings ORDER BY id DESC LIMIT 1")
+                    row = cur.fetchone()
+
+                    now = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                    if row is None:
+                        # Default settings
+                        cur.execute("INSERT INTO settings "
+                        "(Librarian_Name, Issue_Duration,Fines_Per_Day,Max_Book_Issues_Per_User,Password,Date_Time)" \
+                        " VALUES (%s, %s,%s,%s,%s,%s)", 
+                        ('Default',15,0.1,3,"Testing",now))
+                        print("Default Settings created")
+
+                        #Update config.json
+                        new_settings = {
+                                    "Issue_Duration": 15,
+                                    "Fines_Per_Day": 0.1,
+                                    "Max_Book_Issues": 3
+                                }
+                        with open("config.json", "w") as f:
+                            json.dump(new_settings, f, indent=4)
+
+                    else:
+                        while True:  
+                            temp_password = input("Enter Previous Password ")
+                            if temp_password == row[5]:
+                                print("Authorized")
+
+                                #Enter new details
+                                Librarian_Name = input("Enter your name: ")
+                                Issue_Duration = int(input("Enter issue duration: "))
+                                Fines_Per_Day = int(input("Enter Fines Per Day: "))
+                                Max_Issues_Per_User = int(input("Enter Max Book Issues Per User: "))
+                                x = input("Do you want to reset password?(Y/N): ")
+                                if x == 'Y' or x == 'y':
+                                    pwd = input("Enter New Password: ")
+                                else:
+                                    pwd = row[5]
+
+                                cur.execute("INSERT INTO settings "
+                                    "(Librarian_Name, Issue_Duration,Fines_Per_Day,Max_Book_Issues_Per_User,Password,Date_Time)" \
+                                    " VALUES (%s, %s,%s,%s,%s,%s)", 
+                                    (Librarian_Name,Issue_Duration,Fines_Per_Day,Max_Issues_Per_User,pwd,now))
+                                
+                                print("Settings Updated")
+
+                                # Update config.json
+                                new_settings = {
+                                    "Issue_Duration": Issue_Duration,
+                                    "Fines_Per_Day": Fines_Per_Day,
+                                    "Max_Book_Issues": Max_Issues_Per_User
+                                }
+                                with open("config.json", "w") as f:
+                                    json.dump(new_settings, f, indent=4)
+
+                                break
+                            else:
+                                print("Wrong Password")
+                                continue
+
+        case 5: #Export to CSV
+            conn = psycopg2.connect(
+            dbname="library",
+            user="postgres",
+            password="postgress",
+            host="localhost",
+            port="5432"
+            )
+
+            tables = ["users", "books", "transactions"]
+
+            for table in tables:
+                with conn.cursor() as cur:
+                    cur.execute(f"SELECT * FROM {table}")
+                    rows = cur.fetchall()
+                    colnames = [desc[0] for desc in cur.description]
+
+                    with open(f"./data/{table}.csv", "w", newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow(colnames)
+                        writer.writerows(rows)     
+
+            print("Exported all data tables to CSV")
+            conn.close()
 
         case _:
             print("Invalid Input")
--- test_library.py
+++ test_library.py
@@ -1,15 +1,82 @@
 from library import sql_controller
+import json
 
-# pytest test_library.py
+
 def test_sql_controller(): 
 
+    with open("config.json", "r") as config_file:
+        settings = json.load(config_file)
+
     # To test users  
-    test_data = {
+    user_test_data = {
         "User_Name": "KAREN",
         "Contact": 1234,
         'test': True,
     }
-    assert sql_controller("users", 1, test_data) == "KAREN added"
-    assert sql_controller("users", 2, test_data) == "KAREN updated"
-    assert sql_controller("users", 4, test_data) == "KAREN"
-    assert sql_controller("users", 3, test_data) == "KAREN Deleted"
+    try:
+        # Delete data if previous copies exist
+        assert sql_controller("users", 3, user_test_data,settings) == "KAREN Deleted"
+    except:
+        print("No previous copies")
+    finally:
+        # Test all operations
+        assert sql_controller("users", 1, user_test_data,settings) == "KAREN added"
+        assert sql_controller("users", 2, user_test_data,settings) == "KAREN updated"
+        assert sql_controller("users", 4, user_test_data,settings) == {'User_Name': 'KAREN','Contact': 1234,'Books_Borrowed': None,'Fines':None}
+        assert sql_controller("users", 3, user_test_data,settings) == "KAREN Deleted"
+
+    # To test books 
+    book_test_data = {
+        "Book_Name": "HARRY POTTER",
+        "Author_Name": "JK Rowling",
+        "Copies":5,
+        "Available": 0,
+        "Location":"R1C1",
+        'test': True,
+    }
+    
+    try:
+        # Delete data if previous copies exist
+        assert sql_controller("books", 2, book_test_data,settings) == "All Copies of HARRY POTTER Deleted"
+    except:
+        print("No previous copies")
+    finally:
+        # Test all operations
+        assert sql_controller("books", 1, book_test_data,settings) == "HARRY POTTER added"
+        assert sql_controller("books", 3, book_test_data,settings) == {"Book_Name": "HARRY POTTER", "Copies": 5,"Available": 5,"Location": "R1C1","user_id": None}
+        assert sql_controller("books", 2, book_test_data,settings) == "All Copies of HARRY POTTER Deleted"
+
+
+    # To test transactions and overall integration
+    user_data = {
+        "User_Name": "KAREN",
+        "Contact": 1234,
+        'test': True,
+    }
+    book_data = {
+        "Book_Name": "HARRY POTTER",
+        "Author_Name": "JK Rowling",
+        "Copies":5,
+        "Available": 0,
+        "Location":"R1C1",
+        'test': True,
+    }
+    # Check if previous books / users exist to prevent conflicts
+    try:
+        assert sql_controller("books", 2, book_test_data,settings) == "All Copies of HARRY POTTER Deleted"
+    except:
+        print("No previous copies")
+    try:
+        assert sql_controller("users", 3, user_test_data,settings) == "KAREN Deleted"
+    except:
+        print("No previous books")
+
+    # Add books and verify its addition
+    assert sql_controller("books", 1, book_test_data,settings) == "HARRY POTTER added"
+    assert sql_controller("books", 3, book_test_data,settings) == {"Book_Name": "HARRY POTTER", "Copies": 5,"Available": 5,"Location": "R1C1","user_id": None}
+
+    # Add user and verify their addition
+    assert sql_controller("users", 1, user_test_data,settings) == "KAREN added"
+    assert sql_controller("users", 4, user_test_data,settings) == {'User_Name': 'KAREN','Contact': 1234,'Books_Borrowed': None,'Fines':None}
+
+#pytest test_library.py
